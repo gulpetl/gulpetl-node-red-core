0e5c255a0deb671647f98bdbde3d61497e7fc2d0
Peter Lambert <peterl@sbcotton.com>
2023-12-06 01:33:17 -0600

    Moving gulp-info to gulp.dest (from gulp.src) because msg cloning causes odd message duplication

--- gulp_dest.js
+++ gulp_dest.js
@@ -6,7 +6,7 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);        
         this.path = config.path;
         var node = this;
-        node.on('input', function(msg) {
+        node.on('input', function(msg, send) {
             // msg.debug = {};
             // msg.debug.config = config;
             // msg.debug.node = node;
@@ -25,7 +25,28 @@ module.exports = function(RED) {
 
                 console.log(`gulp.dest: creating gulp stream; combining ${msg.plugins.length} plugin streams`)
                 combine(msg.plugins.map((plugin) => plugin.init()))
-                    .pipe(gulp.dest(node.path));
+                    .pipe(gulp.dest(node.path)
+                    .on("data", (file) => {
+                        this.status({ fill: "green", shape: "dot", text: "active" });
+                        // console.log("DATA");
+
+                        // new msg for each file; otherwise we just change the same one and they'll all look the same in the debug pain
+                        let newMsg = msg;//RED.util.cloneMessage(msg);
+
+                        // send an info message to announce the file we're processing
+                        newMsg.payload = "gulpfile: " + file.inspect();
+                        newMsg.topic = "gulp-info";
+                        newMsg.gulpfile = file;
+// send(msg);
+                        send(newMsg);
+                        console.log("gulp.src:", file)
+
+                        // let fileName = file.stem;
+                        // file.contents.on("data", (data) => {
+                        //     console.log("test:" + fileName + ":", data.toString().trim())
+                        // })
+                    })
+                    );
 
                 this.status({fill:"green",shape:"ring",text:"ready"});
             }
--- gulp_src.js
+++ gulp_src.js
@@ -6,18 +6,25 @@ module.exports = function (RED) {
         this.path = config.path;
 
         let node = this;
-console.log("config", config)
+        // console.log("config", config)
 
         node.on('input', function (msg, send) {
             let configObj = { buffer: false, ...msg.config } // default to streaming mode
+            // msg = RED.util.cloneMessage(msg);
+
+            /** 
+             * plugins will be an array of objects where obj.init is a function that returns a stream. This clones well for
+             * when msg is cloned by Node-RED (for passing down multiple wires), unlike arrays of streams or other such options
+             */
             msg.plugins = [];
 
             // set the payload to give info on the gulp stream we're creating
             msg.payload = "gulp.src: " + node.path;
             msg.topic = "gulp-initialize";
 
-console.log("START");
 
+            // when cloning messages takes place (if this node has multiple wires)
+            // it causes duplication of the send(newMsg) below in an odd way.
             msg.plugins.push({
                 name: config.type,
                 // init:() => gulp.src(node.path, configObj)
@@ -25,18 +32,17 @@ console.log("START");
                     return gulp.src(node.path, configObj)
                         .on("data", (file) => {
                             this.status({ fill: "green", shape: "dot", text: "active" });
-                            // console.log("DATA");
 
-                            // new msg for each file; otherwise we just change the same one and they'll all look the same in the debug pain
-                            let newMsg = RED.util.cloneMessage(msg);
+                            // // new msg for each file; otherwise we just change the same one and they'll all look the same in the debug pain
+                            // let newMsg = RED.util.cloneMessage(msg);
 
-                            // send an info message to announce the file we're processing
-                            newMsg.payload = "gulpfile: " + file.inspect();
-                            newMsg.topic = "gulp-info";
-                            newMsg.gulpfile = file;
-// send(msg);
-                            send(newMsg);
-                            console.log("gulp.src:", file)
+                            // // send an info message to announce the file we're processing
+                            // newMsg.payload = "gulpfile: " + file.inspect();
+                            // newMsg.topic = "gulp-info";
+                            // newMsg.gulpfile = file;
+
+                            // send(newMsg);
+                            // console.log("gulp.src:", file)
 
                             // let fileName = file.stem;
                             // file.contents.on("data", (data) => {
@@ -45,13 +51,13 @@ console.log("START");
                         })
                         .on("end", () => {
                             this.status({ fill: "green", shape: "ring", text: "ready" });
-                            // console.log("END")
                         })
                 }
             })
 
             send(msg);
-            console.log('sendMsg')
+
+
         });
 
 
