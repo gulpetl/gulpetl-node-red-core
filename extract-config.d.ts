4c5818d90a22db0970a86190a60cc9466196008a
Peter Lambert <peterl@sbcotton.com>
2024-02-06 21:04:01 -0600

    Add overridableConfigObj

--- extract-config.d.ts
+++ extract-config.d.ts
@@ -4,10 +4,11 @@
 
 /**
  * Merges config information for this plugin from all potential sources
- * @param specificConfigObj A configObj set specifically for this plugin
+ * @param specificConfigObj A configObj set specifically for this plugin. May be an object or a JSON string.
  * @param pipelineConfigObj A "super" configObj (e.g. file.data or msg.config) for the whole pipeline which may/may not apply to this plugin; 
- * only used in absence of specificConfigObj
+ * only used in absence of specificConfigObj. May be an object or a JSON string.
  * @param pluginName Name to search in pipelineConfigObj for settings, i.e. pipelineConfigObj[pluginName]
+ * @param overridableConfigObj like specificConfigObj, but overriden by pipelineConfigObj on a per-property basis.
  * @param defaultConfigObj A default configObj whose properties are overridden by the others
  */
-export declare function extractConfig(specificConfigObj: any, pipelineConfigObj?: any, pluginName?:string, defaultConfigObj?: any): any;
+export declare function extractConfig(specificConfigObj: any, pipelineConfigObj?: any, pluginName?:string, overridableConfigObj?:any, defaultConfigObj?: any): any;
--- extract-config.js
+++ extract-config.js
@@ -6,42 +6,43 @@ const merge = require('merge');
 
 /**
  * Merges config information for this plugin from all potential sources
- * @param specificConfigObj A configObj set specifically for this plugin
- * @param pipelineConfigObj A "super" configObj (e.g. file.data or msg.config) for the whole pipeline which may/may not apply to this plugin; 
- * only used in absence of specificConfigObj
+ * @param specificConfigObj A configObj set specifically for this plugin. May be an object or a JSON string.
+ * @param pipelineConfigObj A "super" configObj (e.g. msg.config or file.data) for the whole pipeline which may/may not apply to this plugin; 
+ * only used in absence of specificConfigObj. May be an object or a JSON string.
  * @param pluginName Name to search in pipelineConfigObj for settings, i.e. pipelineConfigObj[pluginName]
+ * @param overridableConfigObj like specificConfigObj, but overriden by pipelineConfigObj on a per-property basis.
  * @param defaultConfigObj A default configObj whose properties are overridden by the others
  */
-module.exports.extractConfig = function (specificConfigObj, pipelineConfigObj = undefined, pluginName = undefined, defaultConfigObj = undefined) {
-    let configObj;
-    try {
-      let dataObj;
-      if (specificConfigObj) {
-        if (typeof specificConfigObj === 'string')
-          dataObj = JSON.parse(specificConfigObj)
-        else
-          dataObj = specificConfigObj;
-      }
-      else if (pipelineConfigObj) {
-        console.log("typeof pipelineConfigObj: ", typeof pipelineConfigObj)
-        if (typeof pipelineConfigObj === 'string')
-          dataObj = JSON.parse(pipelineConfigObj)
-        else
-          dataObj = pipelineConfigObj;
-
-        // look for a property based on our plugin's name; assumes a complex object meant for multiple plugins
-        if (dataObj[pluginName])
-          dataObj = dataObj[pluginName];
-      }
-  
-      // merge our chosen dataObj into defaultConfigObj, overriding any conflicting properties in defaultConfigObj
-      // merge.recursive(defaultConfigObj, dataObj); // <-- huge bug: can't change parameter objects: changing them affects subsequent plugins in the pipeline
-      configObj = merge.recursive(true, defaultConfigObj, dataObj );
+module.exports.extractConfig = function (specificConfigObj, pipelineConfigObj = undefined, pluginName = undefined, overridableConfigObj = undefined, defaultConfigObj = undefined) {
+  let configObj;
+  try {
+    let dataObj;
+    if (specificConfigObj) {
+      if (typeof specificConfigObj === 'string')
+        dataObj = JSON.parse(specificConfigObj)
+      else
+        dataObj = specificConfigObj;
     }
-    catch (err) { 
-      console.error(err)
+    else if (pipelineConfigObj) {
+      console.log("typeof pipelineConfigObj: ", typeof pipelineConfigObj)
+      if (typeof pipelineConfigObj === 'string')
+        dataObj = JSON.parse(pipelineConfigObj)
+      else
+        dataObj = pipelineConfigObj;
+
+      // look for a property based on our plugin's name; assumes a complex object meant for multiple plugins
+      if (dataObj[pluginName])
+        dataObj = dataObj[pluginName];
     }
 
-    console.log("returning configObj: ", configObj)
-    return configObj;
+    // merge our chosen dataObj into defaultConfigObj, overriding any conflicting properties in defaultConfigObj
+    // merge.recursive(defaultConfigObj, dataObj); // <-- huge bug: can't change parameter objects: changing them affects subsequent plugins in the pipeline
+    configObj = merge.recursive(true, defaultConfigObj, overridableConfigObj, dataObj);
   }
+  catch (err) {
+    console.error(err)
+  }
+
+  console.log("returning configObj: ", configObj)
+  return configObj;
+}
